2f81b1df8cba62dfca3e74a9baeb0a52186d18d2
Nery Estudillo <Estudner000@warren.k12.12.in.us>
2024-08-27 10:12:57 -0400

    INput

---
+++ INput.py
@@ -0,0 +1,4 @@
+name = input("What is your name:  ")
+droids = input("What is the number of droids, ")
+Wookiees = input("What is the number of Wookiees, ")
+print(name, "wants to meet", droids, "droids and ", Wookiees, "Wookiees!")
